3b911fa3bad509be2ba4c18c9675c37bf0f955b0
Muhammed Ömer Faruk Selvi <58203457+mof-selvi@users.noreply.github.com>
2023-12-09 23:37:13 +0300

    Update siniflandirma.py

--- Machine Learning Examples/siniflandirma.py
+++ Machine Learning Examples/siniflandirma.py
@@ -28,3 +28,36 @@ df = pd.DataFrame(data=iris["data"], columns=iris["feature_names"])
 
 df["target"] = iris["target"]
 
+
+
+#%%
+
+from sklearn.utils import shuffle
+df = shuffle(df)
+
+#%%
+
+
+df_np = df.to_numpy()
+
+X = df_np[:,0:4]
+y = df_np[:,4]
+#%%
+from sklearn.model_selection import train_test_split
+
+X_train, X_test, y_train, y_test = train_test_split(X,y, test_size=0.4)
+
+
+
+#%%
+
+from sklearn import svm
+
+model_svm = svm.SVC()
+
+model_svm.fit(X_train, y_train) # train set
+
+
+
+#%%
+model_svm.predict(X_test) # test set
